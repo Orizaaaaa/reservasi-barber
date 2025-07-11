c2d67aad5f2d6c12f3d971fc3d6305d3d7082b7a
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-11 14:24:03 +0700

    finish about capster

--- src/app/about_capster/page.tsx
+++ src/app/about_capster/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import ButtonBack from '@/elements/buttonBack'
 import MobileLayout from '@/fragments/layout/mobileLayout/mobileLayout'
-import { haircut, yangyang2 } from '@/image'
+import { haircut, regular_cut, yangyang2 } from '@/image'
+import { Button } from '@heroui/react'
 import Image from 'next/image'
 import React from 'react'
 import { FaCrown, FaHeart, FaStar } from 'react-icons/fa'
+import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 type Props = {}
 
@@ -11,7 +15,7 @@ const page = (props: Props) => {
     return (
         <section>
 
-            <section className="header w-full bg-primary p-4">
+            <section className="header w-full bg-secondBlack p-4">
                 <ButtonBack />
                 <div className="flex flex-col justify-center items-center">
                     <div className="w-40 h-40">
@@ -22,22 +26,22 @@ const page = (props: Props) => {
                             <div className="p-5 bg-black rounded-full">
                                 <FaHeart color='white' size={27} />
                             </div>
-                            <h1 className='text-white font-bold mt-3' >1370 Cust</h1>
-                            <p className='text-white font-light text-xs' >di layani</p>
+                            <h1 className='text-yellowCustom font-bold mt-3' >1370 Cust</h1>
+                            <p className='text-white font-light text-xs' >Served</p>
                         </div>
                         <div className="box-1 flex flex-col justify-center items-center  ">
                             <div className="p-5 bg-black rounded-full">
                                 <FaCrown color='white' size={27} />
                             </div>
-                            <h1 className='text-white font-bold mt-3' >1370 Cust</h1>
-                            <p className='text-white font-light text-xs' >di layani</p>
+                            <h1 className='text-yellowCustom font-bold mt-3' >10 Years</h1>
+                            <p className='text-white font-light text-xs' >experience</p>
                         </div>
-                        <div className="box-1 flex flex-col justify-center items-center  ">
-                            <div className="p-5 bg-black rounded-full">
+                        <div className="box-1 flex flex-col justify-center items-center   ">
+                            <div className="p-5 bg-black rounded-full ">
                                 <FaStar color='white' size={27} />
                             </div>
-                            <h1 className='text-white font-bold mt-3' >1370 Cust</h1>
-                            <p className='text-white font-light text-xs' >di layani</p>
+                            <h1 className='text-yellowCustom font-bold mt-3' >1370</h1>
+                            <p className='text-white font-light text-xs ' >Rating</p>
                         </div>
                     </div>
                 </div>
@@ -65,20 +69,58 @@ const page = (props: Props) => {
 
             <section className='p-4' >
                 <h1 className='font-semibold mb-3' >Histori</h1>
-                <div className="grid grid-cols-3 lg:grid-cols-5">
-                    <div className="box1 bg-black pb-4 rounded-xl">
-                        <div className="h-52">
-                            <Image className='w-full h-full object-cover rounded-t-xl' src={haircut} alt="logo" />
-                        </div>
-                        <h1 className='text-white text-center mt-3'>Tapper Fade Haircut</h1>
-                        <div className="flex gap-1 justify-center">
-                            <FaStar size={20} color='#E67514' />
-                            <FaStar size={20} color='#E67514' />
-                            <FaStar size={20} color='#E67514' />
-                            <FaStar size={20} color='#E67514' />
-                            <FaStar size={20} color='#E67514' />
-                        </div>
-                    </div>
+                <div className=""> {/* padding untuk kasih ruang kiri-kanan */}
+                    <Swiper
+                        slidesPerView={'auto'}
+                        spaceBetween={16}
+                        pagination={{ clickable: true }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide className="!w-[80%]"> {/* penting: kasih width tetap */}
+                            <div className="border border-grayCustom bg-secondBlack p-2 rounded-xl">
+                                <div className="h-56">
+                                    <Image className="w-full h-full object-cover rounded-lg" src={regular_cut} alt="logo" />
+                                </div>
+                                <div className="flex justify-between items-start w-full mt-2">
+                                    <div className="text">
+                                        <h1 className="text-white">TAPPER FADE HAIRCUT</h1>
+                                        <h1 className="text-white">50K</h1>
+                                    </div>
+
+                                    <div className="flex gap-1">
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide className="!w-[80%]">
+                            <div className="border border-grayCustom bg-secondBlack p-2 rounded-xl">
+                                <div className="h-56">
+                                    <Image className="w-full h-full object-cover rounded-lg" src={regular_cut} alt="logo" />
+                                </div>
+                                <div className="flex justify-between items-start w-full mt-2">
+                                    <div className="text">
+                                        <h1 className="text-white">TAPPER FADE HAIRCUT</h1>
+                                        <h1 className="text-white">50K</h1>
+                                    </div>
+
+                                    <div className="flex gap-1">
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                        <FaStar size={20} color='#E67514' />
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
             </section>
         </section>
