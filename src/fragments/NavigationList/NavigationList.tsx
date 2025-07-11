eb4ee573e7776a86420695330aa6b942c691e312
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-11 16:13:03 +0700

    finish

--- src/fragments/NavigationList/NavigationList.tsx
+++ src/fragments/NavigationList/NavigationList.tsx
@@ -16,7 +16,7 @@ const NavigationList = ({ pathname, title, icon, }: Props) => {
                 href={`${pathname}`}
                 className={`group relative flex items-center gap-2.5 rounded-lg px-4 py-2 font-medium text-bodydark2 duration-300 text-gray-500 
                  ease-in-out hover:bg-primary hover:text-white ${pathnames.includes(pathname) &&
-                    "bg-blue-500/30 text-white"
+                    "bg-yellowCustom/50 text-white"
                     }`}
             >
                 {icon}
