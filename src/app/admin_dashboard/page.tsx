d2cfb9599ca4a6aad5ea20b1eb7d6c434e23d882
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-17 00:28:34 +0700

    finish all

--- src/app/admin_dashboard/page.tsx
+++ src/app/admin_dashboard/page.tsx
@@ -126,7 +126,7 @@ const Page = () => {
 
             <div className="border  border-gray-400 p-3 rounded-2xl">
                 <h1 className='text-xl font-semibold mb-3' >Jumlah Antrian</h1>
-                <div className="grid grid-cols-2 gap-5">
+                <div className="grid grid-cols-2 gap-10">
                     <div className="content">
                         <h1 className='text-lg mb-2'>Capster Yangyang</h1>
                         <hr />
@@ -159,6 +159,27 @@ const Page = () => {
                         <h1 className='text-lg mb-2'>Capster Yangyang</h1>
                         <hr />
                         <h1 className='text-black text-3xl font-extrabold my-5' >5</h1>
+
+                        <div className="overflow-x-auto">
+                            <table className="min-w-full divide-y divide-gray-200 rounded-md shadow-md">
+                                <thead className="">
+                                    <tr>
+                                        <th className="pr-6 py-3 text-left text-sm font-semibold">Nama Customer</th>
+                                        <th className="pr-6 py-3 text-left text-sm font-semibold">Jam</th>
+                                        <th className="pr-6 py-3 text-left text-sm font-semibold">Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {users.map((user) => (
+                                        <tr key={user.jam * 10} className="hover:bg-gray-50 transition">
+                                            <td className="pr-6 py-4 text-sm text-gray-900">{user.name}</td>
+                                            <td className="pr-6 py-4 text-sm text-gray-900">{user.jam + ':00'}</td>
+                                            <td className="pr-6 py-4 text-sm text-gray-900">{user.email}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
 
                     </div>
                 </div>
