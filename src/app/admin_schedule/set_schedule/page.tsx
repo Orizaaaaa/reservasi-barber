467a9297cec7ce2ebcc82a7a7b1e78b73412719e
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-26 00:48:40 +0700

    finish all

---
+++ src/app/admin_schedule/set_schedule/page.tsx
@@ -0,0 +1,15 @@
+import DefaultLayout from '@/fragments/layout/adminLayout/DefaultLayout'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+    return (
+        <DefaultLayout>
+            <div>page</div>
+        </DefaultLayout>
+
+    )
+}
+
+export default page
