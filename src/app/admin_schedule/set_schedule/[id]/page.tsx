a8881615fe1250fd2af5f18a1217a279ffdb72e5
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-27 00:39:24 +0700

    finish

--- src/app/admin_schedule/set_schedule/[id]/page.tsx
+++ src/app/admin_schedule/set_schedule/[id]/page.tsx
@@ -128,39 +128,58 @@ const Page = (props: Props) => {
                                                 onChange={() => handleToggle(item._id)}
                                             />
                                             <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 
-                                                dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 
-                                                peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
-                                                peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
-                                                after:start-[2px] after:bg-white after:border-gray-300 after:border 
-                                                after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 
-                                                peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
+            dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 
+            peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full 
+            peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] 
+            after:start-[2px] after:bg-white after:border-gray-300 after:border 
+            after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 
+            peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600">
                                             </div>
-
                                         </label>
                                     </TableCell>
 
+                                    {/* Jam Kerja */}
                                     <TableCell>
-                                        <div className="flex">
-                                            <h1 className='py-1 px-3 border-black border rounded-md' >10 : 00</h1>
-                                            <h1 className="mx-2">-</h1>
-                                            <h1 className='py-1 px-3 border-black border rounded-md' >10 : 00</h1>
-                                        </div>
+                                        {item.is_active ? (
+                                            <div className="flex">
+                                                <h1 className='py-1 px-3 border-black border rounded-md'>
+                                                    {item.jam_kerja?.split(" - ")[0] || '--:--'}
+                                                </h1>
+                                                <h1 className="mx-2">-</h1>
+                                                <h1 className='py-1 px-3 border-black border rounded-md'>
+                                                    {item.jam_kerja?.split(" - ")[1] || '--:--'}
+                                                </h1>
+                                            </div>
+                                        ) : (
+                                            <div className="text-center text-gray-400">--:--</div>
+                                        )}
                                     </TableCell>
+
+                                    {/* Jam Istirahat */}
                                     <TableCell>
-                                        <div className="flex">
-                                            <h1 className='py-1 px-3 border-black border rounded-md' >10 : 00</h1>
-                                            <h1 className="mx-2">-</h1>
-                                            <h1 className='py-1 px-3 border-black border rounded-md' >10 : 00</h1>
-                                        </div>
+                                        {item.is_active ? (
+                                            <div className="flex">
+                                                <h1 className='py-1 px-3 border-black border rounded-md'>
+                                                    {item.jam_istirahat?.split(" - ")[0] || '--:--'}
+                                                </h1>
+                                                <h1 className="mx-2">-</h1>
+                                                <h1 className='py-1 px-3 border-black border rounded-md'>
+                                                    {item.jam_istirahat?.split(" - ")[1] || '--:--'}
+                                                </h1>
+                                            </div>
+                                        ) : (
+                                            <div className="text-center text-gray-400">--:--</div>
+                                        )}
                                     </TableCell>
+
                                     <TableCell>
-                                        <div>
-                                            <ButtonPrimary className='py-2 w-full rounded-lg' >Simpan</ButtonPrimary>
-                                        </div>
+                                        <ButtonPrimary className='py-2 w-full rounded-lg'>Simpan</ButtonPrimary>
                                     </TableCell>
                                 </TableRow>
                             )}
                         </TableBody>
+
+
                     </Table>
                 </>
             )}
