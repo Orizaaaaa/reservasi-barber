eeac82ae88dce2bf3bb0b455b941b4742e0b0609
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2025-07-29 02:23:30 +0700

    finish

--- src/elements/buttonBack/index.tsx
+++ src/elements/buttonBack/index.tsx
@@ -1,11 +1,14 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import { IoIosArrowBack } from 'react-icons/io'
 
 type Props = {}
 
 function ButtonBack({ }: Props) {
+    const router = useRouter()
     return (
-        <button className='bg-white py-1 px-3 rounded-full flex gap-2 justify-center items-center' >
+        <button onClick={() => router.back()} className='bg-white py-1 px-3 rounded-full flex gap-2 justify-center items-center' >
             <IoIosArrowBack size={20} />
             <h1 className='font-semibold' >Back</h1>
         </button>
